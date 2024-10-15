0f1a51b3667d4a00de2811d9704d159e8bf90a21
Jason Selau Kumar <148664294+Mecha-Coder@users.noreply.github.com>
2024-10-16 07:44:51 +0800

    Syntax validate bracket

--- experiment/token/new/validate/bracket.c
+++ experiment/token/new/validate/bracket.c
@@ -2,58 +2,79 @@
 
 /*
 Bracket unpack by stages (())
-
 ||  &&
 
 Outside
-<START><OPERATOR>  (    )  <OPERATOR><END> 
+<START><OPERATOR>  (    ) <OPERATOR><END> 
 
 Inside
+              ( <STR> )
             (<OB>   <!CB>) 
             (<!OB>   <CB>)           
-
-OK
-echo hello && (echo) && echo hi  ✅
-echo hello && (echo nice && ls) && echo hi ✅
-echo hello && (echo nice && (ls)) && echo hi ❌
-(echo hello) ✅
-echo hello | (cat) ❌
-(ls) ✅
-
-echo hello && (echo hi && (echo good && echo day))
-
-ls && (ls && (ls && (ls && (ls && (ls && (ls)))))) && ls
-
-ERROR
-((echo hello))
-echo hello && (echo (nice)) && echo hi ❌
-echo hello && () && echo hi ❌
-echo hello && (echo nice (ls)) && echo hi ❌
-echo hello && ((echo nice && (ls))) && echo hi
 */
+
+#define OB_INDEX 0
+#define OB_LEFT 1
+#define OB_RIGHT 2
+#define CB_INDEX 3
+#define CB_LEFT 4
+#define CB_RIGHT 5  
+
+
+int find_bracket_pair(t_token *token, int *bracket, int i, int end)
+{
+    int count;
+    int found;
+
+    (found = FALSE, count = 0, bracket[OB_INDEX] = -1, bracket[CB_INDEX] = -1);
+    while(i <= end)
+    {
+        if (token[i].type == OB && count++)
+        {
+            if (count == 1 && bracket[OB_INDEX] == -1)
+            {
+                bracket[OB_INDEX] = i;
+                found = TRUE;
+            }
+        }
+        if (token[i].type == CB && count--)
+        {
+            if (bracket == 0 && bracket[CB_INDEX] == -1)
+                bracket[CB_INDEX] = i;
+        }
+    }
+    return (found);
+}
+
+
+
+
+int recrusive_bracket(t_token *token, int start, int end)
+{
+    int bracket[6];
+    int result;
+
+    if (find_bracket_pair(token, bracket, start, end))
+    {
+        result = recrusive_bracket(token, bracket[OB_INDEX] + 1, bracket[OB_INDEX] -1);
+        if (result >= 0 )
+            return (result);
+        else
+        {
+            result = check_rule(token, bracket, start, end);
+            if (result >= 0)
+                return (result);
+        }       
+    }
+    return (-1);
+}
 
 void bracket(t_data *data, int *return_index)
 {
     int i;
-    int bracket;
-    int left;
-    int right;
-    
-    (i = -1, left = -1, right = -1, bracket = 0);
 
-    while(data->token[++i].type != -1)
-    {
-        if (data->token[i].type == OB)
-        {
-            bracket++;
-            if (bracket == 1 && left == -1)
-                left = i;
-        }
-        if (data->token[i].type == CB)
-        {
-            bracket--;
-            if (bracket == 0 && right == -1)
-                right = i;
-        }
-    }
+    i = 0;
+    while (data->token[i].type != -1)
+        i++;
+    return (recrusive_bracket(data->token, 0, i - 1));
 }
